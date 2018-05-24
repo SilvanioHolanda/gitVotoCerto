b420646268f5f4737d2645949c2aee34739ecfb7
SilvanioHolanda <silvanioholanda197@gmail.com>
2018-05-24 00:08:32 -0300

    teste

--- resources/views/Cadastro-ajuda.blade.php
+++ resources/views/Cadastro-ajuda.blade.php
@@ -15,7 +15,7 @@
 		<div class="fomr">
 		<h1>Ajuda</h1>
 	
-					<label for=""><b>NOME ELEITOR</b></label>
+					<label for=""><b>NOME do ELEITOR</b></label>
 					<input type="text" placeholder="Nome do Eleitor" name="nome_e" id="" required value="{{old('nome_e')}}"[[>
 	
 					<label>ADESIVO</label>
